49667d70c79c344011b08c83f7c3a853b80e6708
jw <always1997@kakao.com>
2022-07-13 14:40:25 +0900

    백준 1244 스위치 켜고 끄기 풀이 코드 수정 완료

--- BOJ/Silver/1244.js
+++ BOJ/Silver/1244.js
@@ -20,8 +20,8 @@ for(let i = 0; i<studentNum; i++) {
     let cur_student_have = parseInt(studentInfo[i].have);
     if(cur_student_gender === 1) {
         for(let j = 0; j<switchNum; j++) { // 남학생
-            if((j+1)%cur_student_have === 0) { // 스위치 번호가 자기가 받은 수의 배수인 경우   
-                state[j] = parseInt(state[j]) === 0 ? 1 : 0; // 상태 변경 
+            if((j+1)%cur_student_have == 0) { // 스위치 번호가 자기가 받은 수의 배수인 경우   
+                state[j] = parseInt(state[j]) == 0 ? 1 : 0; // 상태 변경 
             }
         }
     }else {
@@ -31,40 +31,24 @@ for(let i = 0; i<studentNum; i++) {
 
 
 function changeState(have) {    // 여학생의 경우
-    let startIdx = have;
-    let lastIdx = have;
-    for(let i = 1; i<switchNum/2; i++) {
-        if(parseInt(state[have-i]) == parseInt(state[have+i])) {
-            if(have-i < startIdx) {
-                startIdx = have-i;
-                lastIdx = have+i;
-            }
+    state[have] = (state[have]) == 0 ? 1 : 0;
+    
+    for(let i = 1; i<=switchNum/2; i++) {
+        if((state[have-i]) == (state[have+i])) {
+            state[have-i] = (state[have-i]) == 0 ? 1 : 0;
+            state[have+i] = (state[have+i]) == 0 ? 1 : 0;
+        }else {
+            break;
         }
     } 
-    if(startIdx == have) {
-        state[have] = parseInt(state[have]) == 0 ? 1 : 0;
-    }else {
-        for(let i = startIdx; i<=lastIdx; i++) {
-            state[i] = parseInt(state[i]) == 0 ? 1 : 0;
-        }
-    }
+    
 }
+
 let answer = '';
-let tmpArr = [];
-let count = 0;
-if(state.length> 20) {
-    for(let i = 0; i<state.length/20+1; i++) {
-        let arr = state.splice(count,20*(i+1));
-        count += (20*i);
-        tmpArr.push(arr);
-    }
-}else {
-    answer = state.join(' ');
-}
-if(tmpArr) {
-    for(let i = 0; i<tmpArr.length; i++) {
-        answer += tmpArr[i].join(' ');
-        answer += "\n";
+for(let i = 0; i<state.length; i++) {
+    answer += `${state[i]} `;
+    if((i+1) % 20 == 0) {
+        answer += '\n'
     }
 }
 
