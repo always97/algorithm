e5d84cf7a0c9f24ce1d3f5036992a5a869643f68
jw <always1997@kakao.com>
2022-07-16 23:31:16 +0900

    프로그래머스 내적 풀이 소스코드

--- BOJ/통합입출력예시.js
+++ BOJ/통합입출력예시.js
@@ -1,7 +1,7 @@
 /* 백준에서 node언어 사용하여 문제풀이 하기 위한 기본 형식 (입력, 출력) 예제 */
 
 const fs = require('fs');
-const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
 const input = fs.readFileSync(filePath).toString().split('\n');
 
 console.log(input);
