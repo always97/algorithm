b41c963e6f2a347f49e0c69c0ca141178b2c1603
jw <always1997@kakao.com>
2022-08-24 01:50:15 +0900

    bfs ing

--- BOJ/Bronze/10989.js
+++ BOJ/Bronze/10989.js
@@ -2,4 +2,20 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : '../input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
-console.log(input);
+const [tc,...arr] = input;
+
+let store = new Array(10001).fill(0);
+
+
+for(let i =0; i<tc;i++) {
+  store[Number(arr[i])]++;
+}
+
+for(let i =0; i<10001; i++) {
+  if(store[i]) {
+    for(let j = 0; j<store[i]; j++) {
+      console.log(i);
+    }
+  }
+}
+
