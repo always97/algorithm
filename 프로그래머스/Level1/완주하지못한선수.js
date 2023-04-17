76ea8effa8cbfddb78843fa34a202da58d05d0b7
always97 <always1997@kakao.com>
2023-04-17 22:35:49 +0900

    test

--- 프로그래머스/Level1/완주하지못한선수.js
+++ 프로그래머스/Level1/완주하지못한선수.js
@@ -2,7 +2,6 @@ function solution(participant, completion) {
     var answer = '';
     participant.sort();
     completion.sort();
-    
     for(var i in participant) {
         if(participant[i] !== completion[i]) {
             answer = participant[i];
