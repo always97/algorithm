c6c5d27b4ea9b60e80871d3fd1efba7b474e4f36
always97 <always97@github.com>
2023-04-17 22:22:35 +0900

    완주하지못한선수 풀이 소스코드

---
+++ 프로그래머스/Level1/완주하지못한선수.js
@@ -0,0 +1,14 @@
+function solution(participant, completion) {
+    var answer = '';
+    participant.sort();
+    completion.sort();
+    
+    for(var i in participant) {
+        if(participant[i] !== completion[i]) {
+            answer = participant[i];
+            return answer;
+        }
+    }
+    
+    return answer;
+}
