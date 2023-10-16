91867b6712e8d85d335e555f2dbc981a498049d5
jw <always1997@kakao.com>
2023-10-16 23:59:12 +0900

    SWEA 1859(백만장자) tc에러 해결 소스코드

--- SWEA/D2/1859(백만장자).py
+++ SWEA/D2/1859(백만장자).py
@@ -3,24 +3,14 @@ n = int(input())
 for i in range(1,n+1) :
   nn = int(input())
   data = list(map(int,input().split()))
-  income = [];
+
+  sellPrice = 0;
   outcome = 0;
 
-  for j in range(0,len(data)-1) :
-    if j == len(data)-2 :
-      if data[j] < data[j+1] :
-        outcome += data[j+1]-data[j]
-        if len(income) > 0 : # 구매한게 있다면
-          for k in income :
-            outcome += (data[j+1] - k) # 판매한 수익을 outcome에 더한다
-          income = [] # 모두 판매하면 구매 리스트 초기화
+  for v in data[::-1] :
+    if v >= sellPrice: 
+      sellPrice = v
     else :
-      if data[j] <= data[j+1] :
-        income.append(data[j])
-      else :
-        if len(income) > 0 : # 구매한게 있다면
-          for k in income :
-            outcome += (data[j] - k) # 판매한 수익을 outcome에 더한다
-          income = [] # 모두 판매하면 구매 리스트 초기화
+      outcome += sellPrice - v
 
   print(f'#{i} {outcome}')
