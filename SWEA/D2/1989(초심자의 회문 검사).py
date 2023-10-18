a006265f802be1acee81bb83dd3911734a97b7bc
jw <always1997@kakao.com>
2023-10-18 23:45:17 +0900

    fix: SWEA 1989(초심자의 회문 검사)풀이 소스 코드

--- SWEA/D2/1989(초심자의 회문 검사).py
+++ SWEA/D2/1989(초심자의 회문 검사).py
@@ -4,12 +4,7 @@ for i in range(1,t+1) :
   # 로직
   answer = 0
   data = input()
-
-  print("---------")
-  print(data)
-  print(data[::-1])
-  print("---------")
-
+  
   if data == data[::-1] :
     answer = 1
 
