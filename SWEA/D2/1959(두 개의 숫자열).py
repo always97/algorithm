8af6f0ee2dfab16a5c8f77e61a7b2dec7f5d9621
jw <always1997@kakao.com>
2023-10-19 23:54:03 +0900

    SWEA 1959(두개의 숫자열) 풀이 소스 코드

---
+++ SWEA/D2/1959(두 개의 숫자열).py
@@ -0,0 +1,28 @@
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+
+
+
+for test_case in range(1, T + 1):
+
+  N,M = map(int,(input().split()))
+
+  arrA = list(map(int,(input().split())))
+  arrB = list(map(int,(input().split())))
+
+  if N < M : # 더 큰 값이 N, A가 되도록,
+    N,M = M,N
+    arrA,arrB = arrB,arrA
+
+  abMax = 0
+
+  for i in range(N-M+1) :
+    temp = 0
+    # 작은 값 크기만큼
+    for j in range(M) :
+      temp += arrA[j+i] * arrB[j]
+    if temp > abMax :
+      abMax = temp
+  
+
+  print(f"#{test_case} {abMax}")
