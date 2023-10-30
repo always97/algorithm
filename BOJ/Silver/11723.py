f5bbabb6ac55d349b03798710af2dfd6e447008f
jw <always1997@kakao.com>
2023-10-30 23:58:22 +0900

    백준 11723(집합) remove()에서 발생하는 키에러 discard()로 수정 및 전체 리팩토링

--- BOJ/Silver/11723.py
+++ BOJ/Silver/11723.py
@@ -6,26 +6,24 @@ for i in range(n) :
   com_input = input().split()
 
   if len(com_input) == 1 :
-    control = com_input[0]
-    num = 0
-  elif len(com_input) == 2 :
-    control, num = com_input
-
-
-  if control=='add' :
-    arr.add(int(num))
-  elif control == 'check' :
-    if int(num) in arr :
-      print(1)
-    else : print(0)
-  elif control == 'remove' :
-    arr.remove(int(num))
-  elif control == 'toggle' :
-    if int(num) in arr :
-      arr.remove(int(num))
+    if com_input[0] == 'all' :
+      arr = set([i for i in range(1,21)])
     else :
-      arr.add(int(num))
-  elif control == 'all' :
-    arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20}
+      arr = set()
   else :
-    arr = set()
+    control, num = com_input
+    num = int(num)
+
+    if control=='add' :
+      arr.add(num)
+    elif control == 'remove' :
+      arr.discard(num)
+    elif control == 'check' :
+      if num in arr :
+        print(1)
+      else : print(0)
+    elif control == 'toggle' :
+      if num in arr :
+        arr.discard(num)
+      else :
+        arr.add(num)
