861f2a30cb4f644ecbc9b255243e6462221be817
jw <always1997@kakao.com>
2023-11-16 23:59:44 +0900

    SWEA 2056 출력 형식 수정

--- SWEA/D1/2056(연월일 달력).py
+++ SWEA/D1/2056(연월일 달력).py
@@ -4,13 +4,13 @@ days = [31,28,31,30,31,30,31,31,30,31,30,31];
 
 for i in range(1,n+1) :
   date = input();
-  year = int(date[:4])
-  month = int(date[4:6])
-  day = int(date[6:8])
+  year = (date[:4])
+  month = (date[4:6])
+  day = (date[6:8])
   answer = '-1'
 
-  if 12 >= month >= 1  :
-    if 1 <= day <= days[month-1] :
+  if 12 >= int(month) >= 1  :
+    if 1 <= int(day) <= days[int(month)-1] :
       answer = f"{year}/{month}/{day}"
   
   
