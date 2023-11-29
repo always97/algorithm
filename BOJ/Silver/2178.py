cbd7f871f38027908acd97a3983f0da7029ec8c8
jw <always1997@kakao.com>
2023-11-29 23:56:29 +0900

    백준 2178 bfs 큐 활용 부분 에러 수정

--- BOJ/Silver/2178.py
+++ BOJ/Silver/2178.py
@@ -1,12 +1,12 @@
 n,m = map(int,input().split())
 
-dist = [[-1] for _ in range(n+1)]
+dist = [[-1 for _ in range(102)] for _ in range(102)]
 
 board = []
 
 for i in range(n) :
   input_str = input()
-  board.append([int(j) for j in (input_str)])
+  board.append([int(j) for j in input_str])
 
 # 우 상 좌 하
 dx = [1,0,-1,0] 
@@ -17,15 +17,12 @@ q.append([0,0])
 dist[0][0] = 0
 
 while(q) :
-  x,y = q.pop()
+  x,y = q.pop(0)
   for i in range(4) :
     nx = dx[i] + x
     ny = dy[i]+ y
-    if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 1 :
-        # 방문처리
+    if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 1 and dist[nx][ny] == -1:
       dist[nx][ny] = dist[x][y] + 1
-      board[nx][ny] = 1
-      # board[nx][ny] = 0
       q.append([nx,ny]) 
   
 print(dist[n-1][m-1]+1)
