e66af24d4d49888328388b019dc20b458eca1089
jw <always1997@kakao.com>
2023-11-29 23:58:55 +0900

    백준 2178 bfs 더 간결하게 개선

--- BOJ/Silver/2178.py
+++ BOJ/Silver/2178.py
@@ -1,12 +1,8 @@
 n,m = map(int,input().split())
 
-dist = [[-1 for _ in range(102)] for _ in range(102)]
-
 board = []
-
-for i in range(n) :
-  input_str = input()
-  board.append([int(j) for j in input_str])
+for _ in range(n) :
+  board.append(list(map(int,input())))
 
 # 우 상 좌 하
 dx = [1,0,-1,0] 
@@ -14,16 +10,15 @@ dy = [0,1,0,-1]
 
 q = []
 q.append([0,0])
-dist[0][0] = 0
 
 while(q) :
   x,y = q.pop(0)
   for i in range(4) :
     nx = dx[i] + x
     ny = dy[i]+ y
-    if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 1 and dist[nx][ny] == -1:
-      dist[nx][ny] = dist[x][y] + 1
+    if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == 1 :
+      board[nx][ny] = board[x][y] + 1
       q.append([nx,ny]) 
   
-print(dist[n-1][m-1]+1)
+print(board[n-1][m-1])
       
