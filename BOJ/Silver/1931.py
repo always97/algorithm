564e06b44cbba8238442f2b5b9e23b3ff2403263
jw <always1997@kakao.com>
2024-03-24 22:06:45 +0900

    백준 11724(연결 요소의 개수) 문제 풀이 소스 코드

--- BOJ/Silver/1931.py
+++ BOJ/Silver/1931.py
@@ -3,16 +3,21 @@ n = int(input())
 timeArr = []
 
 for _ in range(n) :
-  s,e = map(int,input().split)
+  s,e = map(int,input().split())
   timeArr.append([s,e])
 
 timeArr.sort(lambda x: (x[1],x[0]))
 
 ans = end = 0
 
-for fs,fe in timeArr :
-  if end <= fs :
+for time in timeArr :
+  if end <= time[0] :
     ans+=1
-    end = fe
+    end = time[1]
 
 print(ans)
+
+
+
+
+
