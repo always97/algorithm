2287c4a54eb3408cbc377fff8e1c6ae1f70f57ee
JunWoo Park <97427744+always97@users.noreply.github.com>
2025-05-15 22:20:52 +0900

    백준 10773(제로) 파이선 풀이 소스 코드

--- BOJ/Silver/10773.py
+++ BOJ/Silver/10773.py
@@ -2,15 +2,12 @@
 
 K = int(input())
 
-num_arr = [];
+num_arr = []
 
 for i in range(K) :
   num = int(input())
-  if i == 0 and num == 0 :
-    pass
   # 0 입력시 가장 최근값 빼기
-  if num == 0 :
-    num_arr.pop()
-    continue
-  num_arr.append(num)
+  if num == 0 : 
+    if num_arr : num_arr.pop()
+  else : num_arr.append(num)
 print(sum(num_arr))
