036e10db02adf7a8afb8c36f1324b8f7f01ec0fb
JunWoo Park <97427744+always97@users.noreply.github.com>
2025-06-03 23:18:13 +0900

    백준 2579 (계단 오르기) 문제 풀이 소스 코드

---
+++ BOJ/Silver/2579.py
@@ -0,0 +1,25 @@
+N = int(input())
+
+stairs = [int(input()) for _ in range(N)]
+
+dp = [0] * N
+
+if N == 0:
+  print(0)
+elif N == 1:
+  print(stairs[0])
+elif N == 2:
+  print(stairs[0] + stairs[1])
+elif N == 3:
+  print(max(stairs[0] + stairs[2], stairs[1] + stairs[2]))
+else:
+  dp[0] = stairs[0]
+  dp[1] = stairs[0] + stairs[1]
+  dp[2] = max(stairs[0] + stairs[2], stairs[1] + stairs[2])
+  
+  for i in range(3, N):
+    score1 = dp[i-2] + stairs[i]
+    score2 = dp[i-3] + stairs[i-1] + stairs[i]
+    dp[i] = max(score1, score2)
+
+  print(dp[N-1])
